7f76677b6f1cdfcf1fd6b348d54d5ba611d9d452
Bhui <Bhui@KI840G8-2.ad.nchecr.unsw.edu.au>
2022-06-22 17:43:11 +1000

    Merge branch 'master' of https://github.com/bbcbh/Package_ClusterModel.git

--- src/population/Population_Bridging_Scheduled.java
+++ src/population/Population_Bridging_Scheduled.java
@@ -177,7 +177,7 @@ public class Population_Bridging_Scheduled extends Population_Bridging {
 			candidates_reg.sort(COMPARATOR_BY_PARTNERSHIP_SOUGHT);
 			candidates_cas.sort(COMPARATOR_BY_PARTNERSHIP_SOUGHT);	
 			
-			// TODO: Current progress - Form partnership on a twelve months schedule
+			// TODO: Current progress - Form partnership on a 12 months schedule
 
 			int[] pop_diff_num_partner_12_months = cal_pop_diff_num_partner(population_num_partner_in_last_12_months);
 			int numCat = pop_diff_num_partner_12_months.length / (1 + LENGTH_GENDER);
