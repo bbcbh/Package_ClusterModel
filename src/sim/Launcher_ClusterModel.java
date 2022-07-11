ec193d84a797bd0edd1c42c384e8fbbf248e3358
Bhui <Bhui@KI840G8-2.ad.nchecr.unsw.edu.au>
2022-07-11 10:18:14 +1000

    Add batch run options for Launcher

--- src/sim/Launcher_ClusterModel.java
+++ src/sim/Launcher_ClusterModel.java
@@ -1,5 +1,8 @@
 package sim;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.InvalidPropertiesFormatException;
@@ -7,10 +10,11 @@ import java.util.InvalidPropertiesFormatException;
 public class Launcher_ClusterModel {
 
 	public static void main(String[] args) throws InvalidPropertiesFormatException, IOException, InterruptedException {
-		
-		final String USAGE_INFO = String.format("Usage: java %s <-gen or -trans> PROP_FILE_DIRECTORY <...>",
-				Launcher_ClusterModel.class.getName());
-		
+
+		final String USAGE_INFO = String.format(
+				"Usage: java %s <-gen or -trans> PROP_FILE_DIRECTORY <...>\n" + "    java %s <-batch> COMMAND_AS_TEXT",
+				Launcher_ClusterModel.class.getName(), Launcher_ClusterModel.class.getName());
+
 		if (args.length < 1) {
 			System.out.println(USAGE_INFO);
 			System.exit(0);
@@ -21,6 +25,16 @@ public class Launcher_ClusterModel {
 				Simulation_ClusterModelGeneration.launch(Arrays.copyOfRange(args, 1, args.length));
 			} else if ("-trans".equals(flag)) {
 				Simulation_ClusterModelTransmission.launch(Arrays.copyOfRange(args, 1, args.length));
+			} else if ("-batch".equals(flag)) {
+				File commands = new File(args[1]);
+				BufferedReader reader = new BufferedReader(new FileReader(commands));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					String[] lines = line.split("\\s+");										
+					Launcher_ClusterModel.main(lines);
+				}
+				reader.close();
+
 			} else {
 				System.out.println(USAGE_INFO);
 				System.exit(0);
