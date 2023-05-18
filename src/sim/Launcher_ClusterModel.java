7e22ef487d696a19e042434220cf13e0fa83aa73
Bhui <Bhui@KIRBYZ240W10-2.ad.nchecr.unsw.edu.au>
2023-05-18 15:33:00 +1000

    Add reg. ex. support in defining of directories used in analysis.

--- src/sim/Launcher_ClusterModel.java
+++ src/sim/Launcher_ClusterModel.java
@@ -2,10 +2,12 @@ package sim;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.InvalidPropertiesFormatException;
+import java.util.regex.Pattern;
 
 import optimisation.Optimisation_Factory;
 import util.Util_Analyse_ClusterModel_Transmission_Output;
@@ -18,7 +20,7 @@ public class Launcher_ClusterModel {
 	public static void main(String[] args) throws InvalidPropertiesFormatException, IOException, InterruptedException {
 
 		final String USAGE_INFO = String.format(
-				"Usage: java %s <-gen, -trans, -opt, -analyse, -analyse_map, -combine_map or -compare> PROP_FILE_DIRECTORY <...>\n"
+				"Usage: java %s <-gen, -trans, -opt, -analyse, -analyse_rx -analyse_map, -combine_map or -compare> PROP_FILE_DIRECTORY <...>\n"
 						+ "or    java %s <-batch> COMMAND_AS_TEXT",
 				Launcher_ClusterModel.class.getName(), Launcher_ClusterModel.class.getName());
 
@@ -39,11 +41,39 @@ public class Launcher_ClusterModel {
 				Optimisation_Factory.stable_prevalence_by_tranmission_fit_GA(Arrays.copyOfRange(args, 1, args.length));
 			} else if ("-analyse".equals(flag)) {
 				Util_Analyse_ClusterModel_Transmission_Output analysis = new Util_Analyse_ClusterModel_Transmission_Output();
-				analysis.setBaseDir(new File(args[1]));
+				File dir = new File(args[1]);
+				analysis.setBaseDir(dir);				
+				System.out.printf("=== %s ===\n", dir.getName());
 				if (args.length > 2) {
 					analysis.setSkipAnalysis(Integer.parseInt(args[2]));
 				}
 				analysis.analyse_outputs();
+			} else if ("-analyse_rx".equals(flag)) {
+				File baseDir = new File(args[1]);
+				if (args.length < 2) {
+					System.out.printf("Usage: java %s -analyse_rx FILE_DIRECTORY PATTERN\n" );
+					System.exit(0);
+				} else {
+					Pattern dirPattern = Pattern.compile(args[2]);
+					File[] candidateDir = baseDir.listFiles(new FileFilter() {
+						@Override
+						public boolean accept(File pathname) {
+							return pathname.isDirectory() && dirPattern.matcher(pathname.getName()).matches();
+						}
+					});
+					System.out.printf("Number of diretories to be analyse = %d\n", candidateDir.length);
+					for (File dir : candidateDir) {
+						System.out.printf("=== %s ===\n", dir.getName());
+						Util_Analyse_ClusterModel_Transmission_Output analysis = new Util_Analyse_ClusterModel_Transmission_Output();
+						analysis.setBaseDir(dir);
+						if (args.length > 3) {
+							analysis.setSkipAnalysis(Integer.parseInt(args[3]));
+						}
+						analysis.analyse_outputs();
+
+					}
+				}
+
 			} else if ("-analyse_map".equals(flag)) {
 				Util_Analyse_ContactMap_Outputs analysis = new Util_Analyse_ContactMap_Outputs(args[1],
 						Integer.parseInt(args[2]), Integer.parseInt(args[3]));
