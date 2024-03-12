91ec90ed502e1bf255ca3848acc37d62f9201834
Bhui <Bhui@KI840G8-2.ad.nchecr.unsw.edu.au>
2024-03-12 19:58:01 +1100

    Revamp and simplifying the network density calculation

--- src/population/Population_Bridging_NetworkDensity.java
+++ src/population/Population_Bridging_NetworkDensity.java
@@ -1,20 +1,17 @@
 package population;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Set;
-
-import org.apache.commons.math3.util.CombinatoricsUtils;
 
 import person.AbstractIndividualInterface;
 import random.MersenneTwisterRandomGenerator;
 import random.RandomGenerator;
 import relationship.ContactMap;
 import sim.Abstract_Runnable_ClusterModel;
-import util.ArrayUtilsRandomGenerator;
 
 public class Population_Bridging_NetworkDensity extends Population_Bridging_Scheduled {
 
@@ -22,16 +19,23 @@ public class Population_Bridging_NetworkDensity extends Population_Bridging_Sche
 
 	// As defined in Karang et al. doi:10.53638/phpma.2017.v5.i1.p15
 	private static final int NETWORK_DENSITY_LVL_3 = 3; // at least 3 sexual relations,
-	private static final int NETWORK_DENSITY_LVL_4 = 4; // at least 4 sexual relations with one common partner
-	private static final int NETWORK_DENSITY_LVL_5 = 5; // at least 5 sexual relations and 2 common partners
-	private static final int NETWORK_DENSITY_LVL_6 = 6; // at least 6 sexual relations where at least 2 common partners
+	private static final int NETWORK_DENSITY_LVL_4 = NETWORK_DENSITY_LVL_3+1; // at least 4 sexual relations with one common partner
+	private static final int NETWORK_DENSITY_LVL_5 = NETWORK_DENSITY_LVL_4+1; // at least 5 sexual relations and 2 common partners
+	private static final int NETWORK_DENSITY_LVL_6 = NETWORK_DENSITY_LVL_5+1; // at least 6 sexual relations where at least 2 common partners
 														// also has partnership
 
 	private static final int[] N_PER_LVL = new int[] { 99, 20, 5, 1 };
 	private static float N_SUM;
 
-
 	private RandomGenerator RNG_density;
+
+	ContactMap cMapLast12Months = new ContactMap();
+
+	HashMap<Integer, int[]> density_record = new HashMap<>(); // Key = PID, V= {Density_Lvl, valid_until}
+
+	private static final int DENSITY_RECORD_LVL = 0;
+	private static final int DENSITY_RECORD_VALID_UNTIL = DENSITY_RECORD_LVL + 1;
+	private static final int LENGTH_DENSITY_RECORD = DENSITY_RECORD_VALID_UNTIL + 1;
 
 	public Population_Bridging_NetworkDensity(long seed) {
 		super(seed);
@@ -41,8 +45,49 @@ public class Population_Bridging_NetworkDensity extends Population_Bridging_Sche
 			nSum += n;
 		}
 		N_SUM = nSum;
-
 		RNG_density = new MersenneTwisterRandomGenerator(this.getSeed());
+
+	}
+
+	@Override
+	protected void checkContactMaps(Integer[] link, ContactMap[] cMaps) {
+		super.checkContactMaps(link, cMaps);
+
+		if (link[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_DURATION] > 0) {
+
+			int link_cMap_expiry_time = link[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_START_TIME]
+					+ link[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_DURATION]
+					+ AbstractIndividualInterface.ONE_YEAR_INT;
+			for (int i = 0; i < 2; i++) {
+				if (!cMapLast12Months.containsVertex(link[i])) {
+					cMapLast12Months.addVertex(link[i]);
+				}
+			}
+			boolean contain_edge = cMapLast12Months.containsEdge(link[0], link[1]);
+
+			if (contain_edge) {
+				Set<Integer[]> existingEdge = cMapLast12Months.getAllEdges(link[0], link[1]);
+				boolean firstEdge = true;
+				for (Integer[] e : existingEdge) {
+					if (firstEdge) {
+						// Replace same edge with longer expiry time
+						int org_cMap_expiry_time = e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_START_TIME]
+								+ e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_DURATION]
+								+ AbstractIndividualInterface.ONE_YEAR_INT;
+						if (link_cMap_expiry_time > org_cMap_expiry_time) {
+							e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_START_TIME] = link[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_START_TIME];
+							e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_DURATION] = link[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_DURATION];
+						}
+						firstEdge = false;
+					} else {
+						// Should only be one edge anyway
+						cMapLast12Months.removeEdge(e);
+					}
+				}
+			} else {
+				cMapLast12Months.addEdge(link[0], link[1], link);
+			}
+		}
 
 	}
 
@@ -50,216 +95,181 @@ public class Population_Bridging_NetworkDensity extends Population_Bridging_Sche
 	public void advanceTimeStep(int deltaT) {
 		super.advanceTimeStep(deltaT);
 
-		ContactMap cMapLast12Months = contactMapInLast12Months(
-				((ContactMap[]) getFields()[FIELD_CONTACT_MAP])[CONTACT_MAP_ALL]);
+		boolean reportPartnerStat = lastPartnershipScheduling < AbstractIndividualInterface.ONE_YEAR_INT
+				? (getGlobalTime() == AbstractIndividualInterface.ONE_YEAR_INT)
+				: getGlobalTime() == lastPartnershipScheduling;
 
-		if (cMapLast12Months != null) {
+		// All Key = PID
+		HashMap<Integer, Integer[]> lookup_partners = new HashMap<>();
+		HashMap<Integer, Integer> lookup_densityLevel = new HashMap<>();
+		HashMap<Integer, Integer> lookup_firstMatchFrom = new HashMap<>();
+		HashMap<Integer, ArrayList<Integer[]>> lookup_no_partnership_between_partners = new HashMap<>();
 
-			HashMap<Integer, ContactMap> lookup_index_connections = new HashMap<>();
-			HashMap<Integer, Integer> lookup_densityLevel = new HashMap<>();
-			HashMap<Integer, Integer> lookup_firstMatchFrom = new HashMap<>();
+		// Clear out all partner that finished 12 months ago
+		for (Integer[] e : cMapLast12Months.edgeSet()) {
+			if (getGlobalTime() > AbstractIndividualInterface.ONE_YEAR_INT
+					&& e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_START_TIME]
+							+ e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_DURATION] < getGlobalTime()
+									- AbstractIndividualInterface.ONE_YEAR_INT) {
+				cMapLast12Months.removeEdge(e);
+			}
+		}
 
-			for (Integer pId : cMapLast12Months.vertexSet()) {
-				if (cMapLast12Months.degreeOf(pId) >= 3) {
-					Set<Integer[]> edgeAll = cMapLast12Months.edgesOf(pId);
-					if (edgeAll.size() >= 3) {
-						ContactMap indivdualMap = new ContactMap();
-						int firstMatchAt = Integer.MAX_VALUE;
-						for (Integer[] e : edgeAll) {
-							firstMatchAt = Math.min(firstMatchAt,
-									e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_START_TIME]);
-
-							if (!indivdualMap.containsVertex(e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P1])) {
-								indivdualMap.addVertex(e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P1]);
-							}
-							if (!indivdualMap.containsVertex(e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P2])) {
-								indivdualMap.addVertex(e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P2]);
-							}
-
-							indivdualMap.addEdge(e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P1],
-									e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P2], e);
-						}
-						lookup_index_connections.put(pId, indivdualMap);
-						lookup_firstMatchFrom.put(pId, firstMatchAt);
+		for (Integer index_pid : cMapLast12Months.vertexSet()) {
+			int[] density_record_ent = density_record.get(index_pid);
+			if (density_record_ent != null && density_record_ent[DENSITY_RECORD_VALID_UNTIL] < getGlobalTime()) {
+				lookup_densityLevel.put(index_pid, density_record_ent[DENSITY_RECORD_LVL]);
+			} else {
+				if (cMapLast12Months.degreeOf(index_pid) >= 3) {
+					if (density_record_ent == null) {
+						density_record_ent = new int[LENGTH_DENSITY_RECORD];
+						density_record.put(index_pid, density_record_ent);
 					}
 
-					Integer[] indexCaseArr = lookup_index_connections.keySet()
-							.toArray(new Integer[lookup_index_connections.size()]);
+					ArrayList<Integer> connection = new ArrayList<>();
+					Set<Integer[]> edgeAll = cMapLast12Months.edgesOf(index_pid);
 
-					for (Integer indexCase : indexCaseArr) {
-						ContactMap indivdualMap = lookup_index_connections.get(indexCase);
-						Set<Integer> vertices = indivdualMap.vertexSet();
-						int density_lvl = NETWORK_DENSITY_LVL_3;
+					int firstMatchAt = Integer.MAX_VALUE;
+					int validUntil = Integer.MAX_VALUE;
+					int density_lvl = NETWORK_DENSITY_LVL_3;
 
-						for (Integer v : vertices) {
-							if (!v.equals(indexCase)) {
-								ArrayList<Integer> third_partner_list = new ArrayList<>();
+					for (Integer[] e : edgeAll) {
+						Integer partner = e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P1].equals(index_pid)
+								? e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P2]
+								: e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P1];
 
-								// Check how many partner of partner is also a direct partner to index case
-								Set<Integer[]> vEdgeAll = cMapLast12Months.edgesOf(v);
-								for (Integer[] e : vEdgeAll) {
-									Integer third_partner = v
-											.equals(e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P1])
-													? e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P2]
-													: e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_P1];
-									if (!third_partner.equals(indexCase)) {
-										boolean isCommonPartner = hasEdgeIn12Months(cMapLast12Months, indexCase,
-												third_partner);
-										if (isCommonPartner) {
-											third_partner_list.add(third_partner);
-											density_lvl = Math.max(density_lvl, NETWORK_DENSITY_LVL_4);
-										}
-									}
+						int pt = Collections.binarySearch(connection, partner);
+						if (pt < 0) {
+							connection.add(~pt, partner);
+						}
+						firstMatchAt = Math.min(firstMatchAt,
+								e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_START_TIME]);
+						validUntil = Math.min(validUntil,
+								e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_START_TIME]
+										+ e[Abstract_Runnable_ClusterModel.CONTACT_MAP_EDGE_DURATION]
+										+ AbstractIndividualInterface.ONE_YEAR_INT);
+					}
 
-								}
-								if (third_partner_list.size() > 1) {
-									density_lvl = Math.max(density_lvl, NETWORK_DENSITY_LVL_5);
-									// Check each partner combination and see if they are partner as well
-									for (int i = 0; i < third_partner_list.size()
-											&& density_lvl < NETWORK_DENSITY_LVL_6; i++) {
-										for (int j = i; j < third_partner_list.size()
-												&& density_lvl < NETWORK_DENSITY_LVL_6; j++) {
-											if (hasEdgeIn12Months(cMapLast12Months, third_partner_list.get(i),
-													third_partner_list.get(j))) {
-												density_lvl = Math.max(density_lvl, NETWORK_DENSITY_LVL_6);
-											}
-										}
-									}
-								}
+					Integer[] partners = connection.toArray(new Integer[0]);
+					ArrayList<Integer[]> no_partnership_array_index = new ArrayList<>();
+
+					for (int p1 = 0; p1 < partners.length; p1++) {
+						for (int p2 = p1 + 1; p2 < partners.length; p2++) {
+							if (!cMapLast12Months.containsEdge(partners[p1], partners[p2])) {
+								no_partnership_array_index.add(new Integer[] { partners[p1], partners[p2] });
+							} else {
+								density_lvl = Math.min(density_lvl + 1, NETWORK_DENSITY_LVL_6);
 							}
 						}
-						lookup_densityLevel.put(indexCase, density_lvl);
 					}
+					lookup_partners.put(index_pid, partners);
+					lookup_no_partnership_between_partners.put(index_pid, no_partnership_array_index);
+					lookup_densityLevel.put(index_pid, density_lvl);
+					lookup_firstMatchFrom.put(index_pid, firstMatchAt);
+
+					density_record_ent[DENSITY_RECORD_LVL] = density_lvl;
+					density_record_ent[DENSITY_RECORD_VALID_UNTIL] = validUntil;
 				}
 			}
+		}
 
-			// Analyse density from individual level
-			if (lookup_densityLevel.size() > N_SUM) {
-				int[] count = new int[N_PER_LVL.length];
-				for (Integer lvl : lookup_densityLevel.values()) {
-					count[lvl.intValue() - NETWORK_DENSITY_LVL_3]++;
-				}
+		// Analyse density from individual level
+		if (lookup_densityLevel.size() > N_SUM) {
 
-				int[] idealCount = new int[N_PER_LVL.length];
-				for (int i = 0; i < idealCount.length; i++) {
-					idealCount[i] = Math.round(lookup_densityLevel.size() * N_PER_LVL[i] / N_SUM);
-				}
-				ArrayList<ArrayList<Integer>> densityLvlCandidates = new ArrayList<>(N_PER_LVL.length);
-				for (int i = 0; i < N_PER_LVL.length; i++) {
-					densityLvlCandidates.add(new ArrayList<>());
-				}
-				for (Integer indexId : lookup_densityLevel.keySet()) {
-					int lvl_pt = lookup_densityLevel.get(indexId).intValue() - NETWORK_DENSITY_LVL_3;
-					ArrayList<Integer> index_arr = densityLvlCandidates.get(lvl_pt);
-					int arrPt = Collections.binarySearch(index_arr, indexId, new Comparator<Integer>() {
-						@Override
-						public int compare(Integer o1, Integer o2) {
-							int res = -Integer.compare(cMapLast12Months.degreeOf(o1), cMapLast12Months.degreeOf(o2));
-							if (res == 0) {
-								res = Integer.compare(o1, o2);
-							}
-							return res;
-						}
-					});
-					if (arrPt < 0) {
-						index_arr.add(~arrPt, indexId);
+			// Calculate current and ideal density count
+			int[] count = new int[N_PER_LVL.length];
+			for (Integer lvl : lookup_densityLevel.values()) {
+				count[lvl.intValue() - NETWORK_DENSITY_LVL_3]++;
+			}
+			int[] idealCount = new int[N_PER_LVL.length];
+			for (int i = 0; i < idealCount.length; i++) {
+				idealCount[i] = Math.round(lookup_densityLevel.size() * N_PER_LVL[i] / N_SUM);
+			}
+
+			// Allocated candidate, order by number of connections
+			ArrayList<ArrayList<Integer>> densityLvlCandidates = new ArrayList<>(N_PER_LVL.length);
+			for (int i = 0; i < N_PER_LVL.length; i++) {
+				densityLvlCandidates.add(new ArrayList<>());
+			}
+			for (Integer index_pid : lookup_densityLevel.keySet()) {
+				int lvl_pt = lookup_densityLevel.get(index_pid).intValue() - NETWORK_DENSITY_LVL_3;
+				ArrayList<Integer> index_arr = densityLvlCandidates.get(lvl_pt);
+				index_arr.add(index_pid);
+			}
+
+			String init_count = Arrays.toString(count);
+			for (int dI = N_PER_LVL.length - 1; dI > 0; dI--) {
+
+				while (idealCount[dI] > count[dI]) {
+					int colSel = 0;
+					while (colSel < densityLvlCandidates.size() && densityLvlCandidates.get(colSel).size() == 0) {
+						colSel++;
 					}
-				}
-				
-				String init_count = Arrays.toString(count);
-				for (int dI = N_PER_LVL.length - 1; dI > 0; dI--) {
-					
-					while (idealCount[dI] > count[dI]) {
-						int colSel = 0;
-						while (colSel < densityLvlCandidates.size() && densityLvlCandidates.get(colSel).size() == 0) {
-							colSel++;
-						}
+
+					if (colSel >= densityLvlCandidates.size()) {
+						// No more suitable candidate
+						break;
+					}
+
+					int indexSel = densityLvlCandidates.get(colSel)
+							.remove(RNG_density.nextInt(densityLvlCandidates.get(colSel).size()));					
+
+					int partnerBeforeTime = lookup_firstMatchFrom.get(indexSel)
+							+ AbstractIndividualInterface.ONE_YEAR_INT;
+
+					if (partnerBeforeTime > getGlobalTime()) {
 						
-						if(colSel >= densityLvlCandidates.size()) {
-							// No more suitable candidate
-							break;
-						}
-						
-						int indexSel = densityLvlCandidates.get(colSel).remove(0);
-						ContactMap indivdual_map = lookup_index_connections.get(indexSel);
+						ArrayList<Integer[]> no_partnership_index = lookup_no_partnership_between_partners.get(indexSel);
+						int numConnectionToAdd = (int) Math.min(dI - colSel, no_partnership_index.size());
 
-						ArrayList<Integer> partner_list = new ArrayList<>(indivdual_map.vertexSet());
-						partner_list.sort(new Comparator<Integer>() {
-							@Override
-							public int compare(Integer o1, Integer o2) {
-								return Integer.compare(o1.intValue(), o2.intValue());
-							}
-						});
-
-						int pt = Collections.binarySearch(partner_list, indexSel);
-						partner_list.remove(pt);
-						Integer[] partner_arr = partner_list.toArray(new Integer[partner_list.size()]);
-
-						int partnerBeforeTime = lookup_firstMatchFrom.get(indexSel)
-								+ AbstractIndividualInterface.ONE_YEAR_INT;
-
-						if (partnerBeforeTime > getGlobalTime()) {
-							int numConnectionToAdd = (int) Math.min(dI - colSel, 
-									CombinatoricsUtils.binomialCoefficient(partner_arr.length, 2));														
+						// Schedule a future partnership to schedule
+						while (numConnectionToAdd > 0) {
 							
-							ArrayList<String> alreadyMatched = new ArrayList<>();
+							Integer[] selectPartner = no_partnership_index
+									.remove(RNG_density.nextInt(no_partnership_index.size()));
+							
+							int partnershipFormTime = getGlobalTime()
+									+ RNG_density.nextInt(partnerBeforeTime - getGlobalTime());
+							Integer[] newEdge = new Integer[] { selectPartner[0], selectPartner[1],
+									CANDIDATE_ARRAY_SOUGHT_ANY};
 
-							// Schedule a future partnership to schedule
-							while (numConnectionToAdd > 0) {
-								Integer[] selectPartner = ArrayUtilsRandomGenerator.randomSelect(partner_arr, 2,
-										RNG_density);
-								Arrays.sort(selectPartner);
-								int sP = Collections.binarySearch(alreadyMatched, Arrays.toString(selectPartner));
-								if (sP < 0) {
-									alreadyMatched.add(~sP, Arrays.toString(selectPartner));
-									int partnershipFormTime = getGlobalTime()
-											+ RNG_density.nextInt(partnerBeforeTime - getGlobalTime());
-									Integer[] newEdge = new Integer[] { selectPartner[0], selectPartner[1],
-											CANDIDATE_ARRAY_SOUGHT_ANY };
-
-									ArrayList<Integer[]> schedule_partnership_ent = schedule_partnership
-											.get(partnershipFormTime);
-									if (schedule_partnership_ent == null) {
-										schedule_partnership_ent = new ArrayList<>();
-										schedule_partnership.put(partnershipFormTime,
-												schedule_partnership_ent);
-									}
-
-									int entPt = Collections.binarySearch(schedule_partnership_ent, newEdge,
-											schedule_partnership_comparator);
-									if (entPt < 0) {
-										schedule_partnership_ent.add(~entPt, newEdge);										
-									}
-									numConnectionToAdd--;
-								}
+							ArrayList<Integer[]> schedule_partnership_ent = schedule_partnership
+									.get(partnershipFormTime);
+							
+							if (schedule_partnership_ent == null) {
+								schedule_partnership_ent = new ArrayList<>();
+								schedule_partnership.put(partnershipFormTime, schedule_partnership_ent);
 							}
+							int entPt = Collections.binarySearch(schedule_partnership_ent, newEdge,
+									schedule_partnership_comparator);
+							if (entPt < 0) {
+								schedule_partnership_ent.add(~entPt, newEdge);
+							}
+							numConnectionToAdd--;
 						}
-						count[dI]++;						
 					}
-
+					count[colSel]--;
+					count[dI]++;
 				}
+			}
 
-				System.out.printf("Time = %d. Unadj. density=%s. Adj. density = %s.\n", 
-						getGlobalTime(), init_count, Arrays.toString(count));
+			System.out.printf("Time = %d. Unadj. density=%s. Adj. density = %s.\n", getGlobalTime(), init_count,
+					Arrays.toString(count));
+			if (reportPartnerStat) {
+				if (printStatus != null) {
+					if (reportPartnerStat) {
+						String str_networkDensity = String.format("Time = %d. Network densitiy = %s", getGlobalTime(),
+								init_count);
+						for (PrintStream pWri : printStatus) {
+							pWri.println(str_networkDensity);
+						}
+					}
+				}
 			}
 
 		}
+
 	}
+
 	
-	private ContactMap contactMapInLast12Months(ContactMap baseMap) {
-		return Abstract_Runnable_ClusterModel.generateContactMapAcrossTimeRange(baseMap, 
-				new int[] {getGlobalTime()- AbstractIndividualInterface.ONE_YEAR_INT, getGlobalTime()});
-	}
-
-	private boolean hasEdgeIn12Months(ContactMap map, int pid_1, int pid_2) {
-		Set<Integer[]> possibleEdges = map.getAllEdges(pid_1, pid_2);
-		if (possibleEdges == null) {
-			return false;
-		} else {
-			return possibleEdges.size() > 0;
-		}
-
-	}
-	
-
 }
