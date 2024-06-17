503c86e5e5db0ee2151295576327bf8f0e1f336e
Bhui <Bhui@KIRBYZ240W10-2.ad.nchecr.unsw.edu.au>
2024-06-17 11:38:30 +1000

    Tidy up import

--- src/sim/Runnable_ClusterModel_Viability.java
+++ src/sim/Runnable_ClusterModel_Viability.java
@@ -3,7 +3,6 @@ package sim;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
