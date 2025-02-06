ac9266978a69538beaee08b53264ff1096677c98
Bhui <Bhui@KI840G8-2.ad.nchecr.unsw.edu.au>
2025-02-06 17:52:34 +1100

    Further progress on Runnable_ClusterModel_ContactMap_Generation_MultiMap (WIP)

--- src/sim/Runnable_ClusterModel_ContactMap_Generation_MultiMap.java
+++ src/sim/Runnable_ClusterModel_ContactMap_Generation_MultiMap.java
@@ -1,7 +1,9 @@
 package sim;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import org.apache.commons.math3.analysis.UnivariateFunction;
 import org.apache.commons.math3.analysis.interpolation.LinearInterpolator;
@@ -15,11 +17,8 @@ public class Runnable_ClusterModel_ContactMap_Generation_MultiMap
 		extends Abstract_Runnable_ClusterModel_ContactMap_Generation {
 
 	// Runnable fields
-	public static final int RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_NUMBER_OF_GRP = LENGTH_RUNNABLE_MAP_GEN_FIELD;
-	public static final int RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_GRP_CANDIDATE = RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_NUMBER_OF_GRP
-			+ 1;
-
-	public static final int RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_AGEING_DIST = RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_GRP_CANDIDATE
+	public static final int RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_NUMBER_OF_GRP = LENGTH_RUNNABLE_MAP_GEN_FIELD;	
+	public static final int RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_AGEING_DIST = RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_NUMBER_OF_GRP+1
 			+ 1;
 
 	public static final int RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_BY_SNAP = RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_AGEING_DIST
@@ -31,20 +30,15 @@ public class Runnable_ClusterModel_ContactMap_Generation_MultiMap
 	public static final Object[] DEFAULT_RUNNABLE_MAP_GEN_MULTIMAP_FIELDS = {
 			// RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_NUMBER_OF_GRP
 			new int[] { 1000 },
-			// RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_GRP_CANDIDATE
-			// 1 << Grp Number
-			new int[] { 1 },
 			// RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_AGEING_DIST
 			// double[GRP_NUMBER]{dist}
 			new double[][] { new double[] { 18 * AbstractIndividualInterface.ONE_YEAR_INT,
 					34 * AbstractIndividualInterface.ONE_YEAR_INT, 0, 1 }, },
+
+			// RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_BY_SNAP		
 			
-			//RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_BY_SNAP
-			// double[GRP_NUMBER][PROB_, 
-			new double[][] {
-						new double[] {}
-			},
-	
+			new double[][] { new double[] { 1, 1, 1, 1, 0.7, 0.7, -2.8 }, },
+
 	};
 
 	// Population Index
@@ -53,6 +47,19 @@ public class Runnable_ClusterModel_ContactMap_Generation_MultiMap
 	private static final int POP_INDEX_ENTER_POP_AT = POP_INDEX_ENTER_POP_AGE + 1;
 	private static final int POP_INDEX_EXIT_POP_AT = POP_INDEX_ENTER_POP_AT + 1;
 	private static final int LENGTH_POP_ENTRIES = POP_INDEX_EXIT_POP_AT + 1;
+	
+	// MAPSETTING
+	// If DUR_FREQ < 0, then it is number of one partnership to form within snapshot
+	// else it is the duration of partnership
+	private static final int MAPSETTING_MAP_TYPE = 0;
+	private static final int MAPSETTING_SNAP_FREQ = MAPSETTING_MAP_TYPE+1;
+	private static final int MAPSETTING_GRP_INDEX_P1 = MAPSETTING_SNAP_FREQ+1;
+	private static final int MAPSETTING_GRP_INDEX_P2 = MAPSETTING_GRP_INDEX_P1+1;
+	private static final int MAPSETTING_PROB_HAS_PARTNERSHIP_P1 =  MAPSETTING_GRP_INDEX_P2+1;
+	private static final int MAPSETTING_PROB_HAS_PARTNERSHIP_P2 =  MAPSETTING_PROB_HAS_PARTNERSHIP_P1 + 1;
+	private static final int MAPSETTING_DUR_FREQ = MAPSETTING_GRP_INDEX_P2+1;
+	
+	
 
 	public static final String MAPFILE_FORMAT = "ContactMap_Type_%d_%d.csv"; // Type, Seed,
 	public static final String POPSTAT_FORMAT = "POP_STAT_%d"; // Seed
@@ -76,31 +83,81 @@ public class Runnable_ClusterModel_ContactMap_Generation_MultiMap
 	@Override
 	public void run() {
 		HashMap<Integer, Object[]> population = new HashMap<>();
+		HashMap<Integer, ArrayList<Integer>> active_in_pop = new HashMap<>();
+
 		int nextId = 1;
+		int popTime = 0;
 		int[] numInGrp = (int[]) runnable_fields[RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_NUMBER_OF_GRP];
+		double[][] ageDist = (double[][]) runnable_fields[RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_AGEING_DIST];
 
 		UnivariateInterpolator polator = new LinearInterpolator();
 
+		// Reuse variables
+		ArrayList<Integer> active_by_grp;
+
 		// Initialise pop
-		for (int g = 0; g < numInGrp.length; g++) {
-			double[] ageDist = (double[]) runnable_fields[RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_AGEING_DIST];
+		for (int g = 0; g < numInGrp.length; g++) {			
+			UnivariateFunction ageDistFunc = polator.interpolate(Arrays.copyOf(ageDist[g], ageDist[g].length / 2),
+					Arrays.copyOfRange(ageDist[g], ageDist[g].length / 2, ageDist[g].length));
 
-			UnivariateFunction ageDistFunc = polator.interpolate(Arrays.copyOf(ageDist, ageDist.length / 2),
-					Arrays.copyOfRange(ageDist, ageDist.length / 2, ageDist.length));
-
+			active_by_grp = new ArrayList<>();
 			for (int i = 0; i < numInGrp[g]; i++) {
 				Object[] newPerson = new Object[LENGTH_POP_ENTRIES];
 				newPerson[POP_INDEX_GRP] = g;
 				newPerson[POP_INDEX_ENTER_POP_AT] = 1;
 				newPerson[POP_INDEX_ENTER_POP_AGE] = (int) Math.round(ageDistFunc.value(RNG.nextDouble()));
-				newPerson[POP_INDEX_EXIT_POP_AT] = ((int) ageDist[ageDist.length / 2]
+				newPerson[POP_INDEX_EXIT_POP_AT] = ((int) ageDist[g][ageDist[g].length / 2]
 						- (int) newPerson[POP_INDEX_ENTER_POP_AGE]) + (int) newPerson[POP_INDEX_ENTER_POP_AT];
-				// Undefined
-
 				population.put(nextId, newPerson);
 
+				active_by_grp.add(nextId);
 				nextId++;
 			}
+			active_in_pop.put(g, active_by_grp);
+
+		}
+
+		for (int snapC = 0; snapC < numSnaps; snapC++) {
+			popTime += snapFreq;
+			
+			for (int g = 0; g < numInGrp.length; g++) {
+				active_by_grp = active_in_pop.get(g);
+				int numRemoved = 0;
+
+				Iterator<Integer> iter = active_by_grp.iterator();
+				while (iter.hasNext()) {
+					int pId = iter.next();
+					Object[] perStat = population.get(pId);
+					// Remove age out person
+					if ((int) perStat[POP_INDEX_EXIT_POP_AT] <= popTime) {
+						iter.remove();
+						numRemoved++;
+					}
+					// Add new person
+					while(numRemoved > 0) {						
+						Object[] newPerson = new Object[LENGTH_POP_ENTRIES];
+						newPerson[POP_INDEX_GRP] = g;
+						newPerson[POP_INDEX_ENTER_POP_AT] = popTime - RNG.nextInt(snapFreq) ;
+						newPerson[POP_INDEX_ENTER_POP_AGE] = (int) ageDist[g][0];
+						newPerson[POP_INDEX_EXIT_POP_AT] = ((int) ageDist[g][ageDist.length / 2]
+								- (int) newPerson[POP_INDEX_ENTER_POP_AGE]) + (int) newPerson[POP_INDEX_ENTER_POP_AT];
+						population.put(nextId, newPerson);
+						active_by_grp.add(nextId);
+						nextId++;													
+						numRemoved--;
+					}
+				}																	
+				
+			}
+			
+			for(double[] map_setting :  (double[][]) runnable_fields[RUNNABLE_FIELD_CONTACT_MAP_GEN_MULTIMAP_PARTNERSHIP_BY_SNAP]) {				
+				// TODO: Generate map
+				int map_type = (int) map_setting[MAPSETTING_MAP_TYPE];
+			}
+			
+			
+			
+		
 
 		}
 
